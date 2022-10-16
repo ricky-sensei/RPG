118039893dd983560381526bf4dce331ef7d40f0
ricky-sensei <axcelcamp.teacher@gmail.com>
2022-10-16 11:38:48 +0900

    first commit

--- main.py
+++ main.py
@@ -7,34 +7,70 @@ white = (255, 255, 255)
 red = (255, 0, 0)
 
 def main():
-    pygame.init()                                   # Pygameの初期化
-    screen = pygame.display.set_mode((800,600))    # 400 x 300の大きさの画面を作る
-    pygame.display.set_caption("いけめんがつくったげえむ")              # 画面上部に表示するタイトルを設定
+    # Pygameの初期化
+    pygame.init()
+    # 400 x 300の大きさの画面を作る
+    screen = pygame.display.set_mode((800,600))
+    # 画面上部に表示するタイトルを設定
+    pygame.display.set_caption("いけめんがつくったげえむ")
+
+    # 画像をフォルダからロードする
     slyme_image = pygame.image.load("image/slyme.png")
+    # フォントをフォルダからロードする
     font = pygame.font.Font("font/DragonQuestFC.ttf", 30)
 
-    button = pygame.Rect(100, 100, 150, 150)
+    base_rect = pygame.Rect(20, 400, 760, 180)
+    menu_height = 100 / 3
+    menu_width = 580
+    menu_between = 20
+
+    # メニューボタンの作成
+    menu1_rect = pygame.Rect(110, 400 + menu_between, menu_width, menu_height)
+    menu2_rect = pygame.Rect(110, 400 + menu_between * 2 + menu_height, menu_width, menu_height)
+    menu3_rect = pygame.Rect(110, 400 + menu_between * 3 + menu_height * 2, menu_width, menu_height)
+    # メニューボタンの文字を作成
+    menu1_textBattle = font.render("たたかう", True, white)
+    menu2_textBattle = font.render("じゅもん", True, white)
+    menu3_textBattle = font.render("にげる", True, white)
 
 
     while (1):
+        """
+        大きい四角をbase
+            rectオブジェクトをbase_rect
+        選択肢のボタンをmenu1, menu2, menu3
+            rectオブジェクトをmenu_rect
+            表示テキストをmenu1_battle, menu2_magic, menu3_run
+            それぞれ たたかう まほう にげる
+            
+        """
         screen.fill(black)  # 画面を黒色に塗りつぶし
+        # スライムの表示
         screen.blit(slyme_image, (400 - (400 / 2), 0))
 
-        pygame.draw.rect(screen, white, pygame.Rect(20, 400, 760, 180), 10)
+        pygame.draw.rect(screen, white, base_rect, 10)
         text = font.render("りっきーが　あらわれた！", True, white)
         screen.blit(text, (40, 420))
 
-        pygame.draw.rect(screen, red, button)
+
+        # メニューボタンを表示
+        pygame.draw.rect(screen, red, menu1_rect)
+        pygame.draw.rect(screen, red, menu2_rect)
+        pygame.draw.rect(screen, red, menu3_rect)
+        screen.blit(menu1_textBattle, (menu1_rect.x, menu1_rect.y))
+        screen.blit(menu2_textBattle, (menu2_rect.x, menu2_rect.y))
+        screen.blit(menu3_textBattle, (menu3_rect.x, menu3_rect.y))
+
 
         pygame.display.update()     # 画面を更新
         # イベント処理
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print(event.pos)
-                if button.collidepoint(event.pos):
-                    print("あたり！")
-                else:
-                    print("ばー－－－－－－－－－か！目んたまついてんのかｗｗｗｗ")
+                # if button.collidepoint(event.pos):
+                #     print("あたり！")
+                # else:
+                #     print("ばー－－－－－－－－－か！目んたまついてんのかｗｗｗｗ")
 
             if event.type == pygame.QUIT:  # 閉じるボタンが押されたら終了
                 pygame.quit()       # Pygameの終了(画面閉じられる)
--- test.py
+++ test.py
@@ -7,34 +7,20 @@ def main():
     screen = pygame.display.set_mode((800,600))    # 400 x 300の大きさの画面を作る
     pygame.display.set_caption("いけめんがつくったげえむ")              # 画面上部に表示するタイトルを設定
 
+    rect_x = 20 #長方形左上のｘ座標
+    rect_y = 400  #長方形左上のｘ座標
+    rect_width = 800 - (rect_x * 2)  #長方形の幅
+    rect_height = 600 - (rect_y + 20) #長方形の高さ
     slyme_image = pygame.image.load("image/slyme.png")
 
+    font = pygame.font.Font("font/DragonQuestFC.ttf", 30)
     while (1):
         screen.fill((0,0,0))  # 画面を黒色に塗りつぶし
         screen.blit(slyme_image, (400 - (400 / 2), 0))
 
-        pygame.display.update()     # 画面を更新
-        # イベント処理
-        for event in pygame.event.get():
-            if event.type == QUIT:  # 閉じるボタンが押されたら終了
-                pygame.quit()       # Pygameの終了(画面閉じられる)
-                sys.exit()
-
-
-if __name__ == "__main__":
-    main()
-
-import pygame
-from pygame.locals import *
-import sys
-
-def main():
-    pygame.init()                                   # Pygameの初期化
-    screen = pygame.display.set_mode((400, 300))    # 400 x 300の大きさの画面を作る
-    pygame.display.set_caption("クソゲー")              # 画面上部に表示するタイトルを設定
-
-    while (1):
-        screen.fill((0,0,0))        # 画面を黒色に塗りつぶし
+        pygame.draw.rect(screen,(255, 255, 255), Rect(rect_x, rect_y, rect_width, rect_height), 10)
+        text = font.render("りっきーが　あらわれた！", True, (255, 255, 255))
+        screen.blit(text, (40, 420))
         pygame.display.update()     # 画面を更新
         # イベント処理
         for event in pygame.event.get():
